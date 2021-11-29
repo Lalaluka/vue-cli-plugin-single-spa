1757d8c25a19dea5203b1863ad065ea824cbefca
calvin.schroeder@telekom.de <calvin.schroeder@telekom.de>
2021-11-29 14:19:40 +0100

    move to npm

--- test/testVue.sh
+++ test/testVue.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+Version=`npx -c 'node -p "process.env.npm_package_version"'`
+tgz="../../vue-cli-plugin-single-spa-"$Version".tgz"
 mkcdir ()
 {
     mkdir -p -- "$1" &&
@@ -10,6 +11,8 @@ if [ $1 == 2 ] || [ $1 == 3 ];
 then
   ProjectName="test-app-vue$1"
 
+  pnpm pack --pack-destination test
+
   mkcdir test/fixtures
 
   cleanup() (
@@ -19,15 +22,15 @@ then
 
   echo $ProjectName
 
-  vue create $ProjectName --no-git --inlinePreset "{\"useConfigFiles\": true,\"plugins\": {},\"vueVersion\": \"$1\"}" --packageManager pnpm
+  vue create $ProjectName --no-git --inlinePreset "{\"useConfigFiles\": true,\"plugins\": {},\"vueVersion\": \"$1\"}" --packageManager npm
 
   cd $ProjectName
 
-  pnpm add ../../../ -D || ERRCODE=$?
+  npm install $tgz
   yes Y | vue invoke single-spa || ERRCODE=$?
-  pnpm run build || ERRCODE=$?
+  npm run build || ERRCODE=$?
 
-  #cleanup
+  cleanup
   exit $ERRCODE
 else
   echo "$1 is no valid Vue Version"
